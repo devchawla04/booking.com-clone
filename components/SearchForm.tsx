1cd0d55fa09650655b18e11e7b4f5f8c01dea091
DevChawla <119198647+DevChawla@users.noreply.github.com>
2025-03-13 21:15:03 +0530

    Home Page UI

---
+++ components/SearchForm.tsx
@@ -0,0 +1,227 @@
+"use client";
+import { useRouter } from "next/router";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { date, z } from "zod";
+import { Button } from "@/components/ui/button";
+import { format } from "date-fns";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { BedDoubleIcon, CalendarIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+export const formSchema = z.object({
+  location: z
+    .string()
+    .min(2, "String must contain atleast 2 characters")
+    .max(50),
+  dates: z.object({ from: z.date(), to: z.date() }),
+  adults: z
+    .string()
+    .min(1, {
+      message: "Please select at least 1 adult",
+    })
+    .max(12, { message: "Max 12 adults Occupancy" }),
+  children: z.string().min(0).max(12, {
+    message: "Max 12 children Occupancy",
+  }),
+  rooms: z.string().min(1, {
+    message: "Please select at least 1 room",
+  }),
+});
+
+const SearchForm = () => {
+  //const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      location: "",
+      dates: {
+        from: undefined,
+        to: undefined,
+      },
+      adults: "1",
+      children: "0",
+      rooms: "1",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col lg:flex-row lg:max-w-6xl lg:mx-auto items-center justify-center space-x-0 lg:space-x-2 space-y-4 lg:space-y-0 rounder-lg"
+        >
+          <div className="grid w-full lg:max-w-sm items-center gap-1.5">
+            <FormField
+              control={form.control}
+              name="location"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-white flex">
+                    Location
+                    <BedDoubleIcon className="ml-2 h-4 w-4 text-white" />
+                  </FormLabel>
+
+                  <FormMessage />
+
+                  <FormControl>
+                    <Input
+                      className="bg-white"
+                      placeholder="London, UK"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            ></FormField>
+          </div>
+          <div className="grid w-full lg:max-w-sm flex-1 items-center gap-1.5">
+            <FormField
+              control={form.control}
+              name="dates"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel className="text-white">Dates</FormLabel>
+
+                  <FormMessage />
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          id="date"
+                          variant={"outline"}
+                          className={cn(
+                            "w-full justify-start text-left font-normal",
+                            !field.value.from && "text-muted-foreground"
+                          )}
+                        >
+                          <CalendarIcon />
+                          {field.value.from ? (
+                            field.value.to ? (
+                              <>
+                                {format(field.value.from, "LLL dd, y")} -{" "}
+                                {format(field.value.to, "LLL dd, y")}
+                              </>
+                            ) : (
+                              format(field.value.from, "LLL dd, y")
+                            )
+                          ) : (
+                            <span>Select your dates</span>
+                          )}
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        initialFocus
+                        mode="range"
+                        defaultMonth={field.value?.from}
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        numberOfMonths={2}
+                        disabled={(date: Date) =>
+                          date < new Date(new Date().setHours(0, 0, 0, 0))
+                        }
+                      />
+                    </PopoverContent>
+                  </Popover>
+                </FormItem>
+              )}
+            ></FormField>
+          </div>
+
+          <div className="flex w-full items-center space-x-2">
+            <div className="grid items-center flex-1">
+              <FormField
+                control={form.control}
+                name="adults"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel className="text-white">Adults</FormLabel>
+                    <FormMessage></FormMessage>
+                    <FormControl>
+                      <Input
+                        className="bg-white"
+                        type="number"
+                        placeholder="Adults"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              ></FormField>
+            </div>
+            <div className="grid items-center flex-1">
+              <FormField
+                control={form.control}
+                name="adults"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel className="text-white">Children</FormLabel>
+                    <FormMessage></FormMessage>
+                    <FormControl>
+                      <Input
+                        className="bg-white"
+                        type="number"
+                        placeholder="Children"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              ></FormField>
+            </div>
+            <div className="grid items-center flex-1">
+              <FormField
+                control={form.control}
+                name="rooms"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel className="text-white">Rooms</FormLabel>
+                    <FormMessage></FormMessage>
+                    <FormControl>
+                      <Input
+                        className="bg-white"
+                        type="number"
+                        placeholder="Rooms"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              ></FormField>
+            </div>
+
+            <div className="mt-auto">
+              <Button type="submit" className="bg-blue-500 text-base">
+                Search
+              </Button>
+            </div>
+          </div>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default SearchForm;
---
+++ data/trending.ts
@@ -0,0 +1,39 @@
+import { Description } from "@headlessui/react";
+
+export const trending_data = [
+    {
+        id:1,
+        src: "https://r-xx.bstatic.com/xdata/images/city/526x420/977220.jpg?k=ee4b7b42c35b8cbf09c8ddb7630092b40cd706fec153c41904ed6e252a883938&0=%22",
+        title:"Dubai",
+        location: "United Arab Emirates",
+        Description: "15 deals"
+    },
+    {
+        id:2,
+        src: "https://cf.bstatic.com/xdata/images/xphoto/540x405/288594543.webp?k=7a96a2b4190146f63068bd0604a916c0c885c4899a527e24a9b39487d4ae50b8&0=%22",
+        title:"South Korea",
+        location: "Asia",
+        Description: "32 deals"
+    },
+    {
+        id:3,
+        src: "https://cf.bstatic.com/xdata/images/xphoto/540x405/289320924.webp?k=99a00f2907495aaeb6396695c053e3d8b95fb05619b10e76c89fb1f7d1fec427&0=%22",
+        title:"London",
+        location: "United Kingdom",
+        Description: "45 deals"
+    },
+    {
+        id:4,
+        src: "https://cf.bstatic.com/xdata/images/xphoto/540x405/290483794.webp?k=916f7bac0ccdb08efcb269ad29cc10816ab66cd1671359066d23d32fb17b5c39&0=%22",
+        title:"Singapore",
+        location: "Asia",
+        Description: "88 deals"
+    },
+    {
+        id:5,
+        src: "https://cf.bstatic.com/xdata/images/xphoto/540x405/173724501.webp?k=5e8a2353c6cd4efef3c5f992ea36d65598c52f2662d45cedc460d1a6a759109f&o=%22",
+        title:"New York City",
+        location: "United States of America",
+        Description: "92 deals"
+    },
+]
